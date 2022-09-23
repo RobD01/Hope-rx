0c52e0485ef6603683b6d2dc9e8a243513c3da2b
RobD01 <medbz2k3@gmail.com>
2022-09-23 12:56:19 -0700

    fixed map

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,12 @@
 class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
+
+    
     <footer class="footer">
       <div class="footer-container">
         <div class="social">
-          <a href="https://instagram.com/xuanloanfabrics" target="_blank"
+          <a href="" 
             ><img
               src="https://1000logos.net/wp-content/uploads/2017/02/Instagram-Logo.png"
               alt="Instagram"
